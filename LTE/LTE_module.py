c2f644cef7ec39bda2fd20247e256f0cdc533848
HendProfessor <projectstar.studio@yandex.ru>
2020-10-30 00:26:53 +0300

    Update LTE_module.py

--- LTE/LTE_module.py
+++ LTE/LTE_module.py
@@ -1,25 +1,25 @@
-from Cryptodome import Hash
+from Cryptodome import Hash, Random, PublicKey, Protocol, IO, Cipher, Math, Signature, SelfTest, Util
 from Cryptodome.Hash import SHA256
-from Cryptodome import Random
-#import Crypto
 
-class Encryption():
+
+class Encryption:
     """
     Encryption
     """
 
-    def LTE_1(object_data):
+    def LTE_1(self, object_data):
         """
         LTE 1
         """ 
-        object_data = Hash.SHA256.new(data = b"AS")
+        object_data = Hash.SHA256.new(data = b"P")
         print(object_data.digest())
-        #raise object_data
-        return object_data
+        #self.e= object_data
+
 
     
 
-Encryption.LTE_1
+Encryption.LTE_1()
+
 #print(Encryption.LTE_1(object_data=b"WW#"))
 
 
