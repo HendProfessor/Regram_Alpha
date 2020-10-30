b43877bd27203ef9d2425f34cccf73b256a8499a
HendProfessor <projectstar.studio@yandex.ru>
2020-10-30 18:19:13 +0300

    Update

--- LTE/LTE_module.py
+++ LTE/LTE_module.py
@@ -1,5 +1,7 @@
-from Cryptodome import Hash, Random, PublicKey, Protocol, IO, Cipher, Math, Signature, SelfTest, Util
-from Cryptodome.Hash import SHA256
+# pip install pycryptodomex
+# from Cryptodome import Hash, Random, PublicKey, Protocol, IO, Cipher, Math, Signature, SelfTest, Util
+import Cryptodome.
+
 
 
 class Encryption:
@@ -7,18 +9,19 @@ class Encryption:
     Encryption
     """
 
-    def LTE_1(self, object_data):
+    def LTE_1(self):
         """
         LTE 1
         """ 
         object_data = Hash.SHA256.new(data = b"P")
         print(object_data.digest())
         #self.e= object_data
-
-
     
 
-Encryption.LTE_1()
+Encryption.LTE_1
+
+wy=Hash.SHA256.new(data = b"009")
+print(wy.digest())
 
 #print(Encryption.LTE_1(object_data=b"WW#"))
 
